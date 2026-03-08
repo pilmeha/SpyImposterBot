86a5f5f1d437504c8f2ce5bca2de2f30c7c7b8b6
pilmeha <garmayev.ayur@inbox.ru>
2026-03-09 01:49:36 +0500

    тестовый код для проверки бота

--- SpyImposterBot/Program.cs
+++ SpyImposterBot/Program.cs
@@ -1,10 +1,48 @@
-﻿namespace SpyImposterBot
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using DotNetEnv;
+
+Env.Load();
+
+var botToken = Environment.GetEnvironmentVariable("BOT_TOKEN")
+    ?? throw new InvalidOperationException("BOT_TOKEN not set");
+
+using var cts = new CancellationTokenSource();
+var bot = new TelegramBotClient(botToken, cancellationToken: cts.Token);
+var me = await bot.GetMe();
+bot.OnError += OnError;
+bot.OnMessage += OnMessage;
+bot.OnUpdate += OnUpdate;
+
+Console.WriteLine($"@{me.Username} is running... Press Enter to terminate");
+Console.ReadLine();
+cts.Cancel(); // stop the bot
+
+// method to handle errors in polling or in your OnMessage/OnUpdate code
+async Task OnError(Exception exception, HandleErrorSource source)
 {
-    internal class Program
+    Console.WriteLine(exception); // just dump the exception to the console
+}
+
+// method that handle messages received by the bot:
+async Task OnMessage(Message msg, UpdateType type)
+{
+    if (msg.Text == "/start")
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        await bot.SendMessage(msg.Chat, "Welcome! Pick one direction",
+            replyMarkup: new InlineKeyboardButton[] { "Left", "Right" });
+    }
+}
+
+// method that handle other types of updates received by the bot:
+async Task OnUpdate(Update update)
+{
+    if (update is { CallbackQuery: { } query }) // non-null CallbackQuery
+    {
+        await bot.AnswerCallbackQuery(query.Id, $"You picked {query.Data}");
+        await bot.SendMessage(query.Message!.Chat, $"User {query.From} clicked on {query.Data}");
     }
 }
