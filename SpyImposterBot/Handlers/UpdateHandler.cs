1515f30795673a803b71d1d0e09d08c5c95979a1
pilmeha <garmayev.ayur@inbox.ru>
2026-03-27 12:44:15 +0500

    добавил кнопки выбора кол-во игроков

--- SpyImposterBot/Handlers/UpdateHandler.cs
+++ SpyImposterBot/Handlers/UpdateHandler.cs
@@ -5,6 +5,7 @@ using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
 
 
 internal class UpdateHandler
@@ -27,12 +28,35 @@ internal class UpdateHandler
         if (update.Type == UpdateType.Message)
         {
             var msg = update.Message!;
+            var chatId = msg.Chat.Id;
+            var keyboard1 = new ReplyKeyboardMarkup(
+                new[]
+                {
+                    new KeyboardButton[] {"Первая кнопка", "Вторая кнопка" },
+                    new KeyboardButton[] { "Третья кнопка " },
+                }
+            )
+            {
+                ResizeKeyboard = true
+            };
+
+            var keyboardPlayerCount = new InlineKeyboardMarkup(
+                new[]
+                {
+                    new InlineKeyboardButton[] { "3", "4", "5", "6"},
+                    new InlineKeyboardButton[] { "7", "8", "9", "10" },
+                }
+            );
 
             if (msg.Text == "/start")
             {
                 //await _userService.CreateUser(msg.From!.Id, msg.From.Username);
 
                 await bot.SendMessage(msg.Chat, "Привет! Это игра в шпиона");
+
+                //await bot.SendMessage(msg.Chat, "Какой-то текст для 1го типа кнопок", replyMarkup: keyboard1);
+
+                await bot.SendMessage(msg.Chat, "Выбери количество игроков!", replyMarkup: keyboardPlayerCount);
             }
         }
     }
